e0551b63c3ab27dcd6bed1530765069bb3eb37ee
KangJuSeong <wntjd3686@gmail.com>
2022-03-25 18:24:37 +0900

    add loop in client.py

--- client.py
+++ client.py
@@ -5,13 +5,15 @@ IP = "127.0.0.1"
 PORT = 10000
 SIZE = 1024
 
-with socket(AF_INET, SOCK_STREAM) as client_socket:
-    client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 바인딩
+while True:
+    with socket(AF_INET, SOCK_STREAM) as client_socket:
+        client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 바인딩
 
-    method = input("Input method (GET, POST, PUT, HEAD): ")
-    url = input('Input URL : ')
-    client_socket.send((method + ',' + url).encode('utf-8'))
-    print("Sending Request to Server")
+        method = input("Input method (GET, POST, PUT, HEAD): ")
+        url = input('Input URL : ')
+        client_socket.send((method + ',' + url).encode('utf-8'))
+        print("Sending Request to Server")
 
-    data = client_socket.recv(SIZE).decode('utf-8')
-    print("Response data : ", data)
+        data = client_socket.recv(SIZE).decode('utf-8')
+        print("Response data : ", data)
+        client_socket.close()
