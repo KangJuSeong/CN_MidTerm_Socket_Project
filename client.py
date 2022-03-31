166bb582a24e728f2103651de9c54dc49a7433ed
KangJuSeong <wntjd3686@gmail.com>
2022-03-31 15:42:51 +0900

    change response and request format + modifing http method logic

--- client.py
+++ client.py
@@ -1,19 +1,30 @@
 from socket import *
 
+format = """POST / HTTP/1.1\n
+            Host: 127.0.0.1:10000
+            Accept: text/html
+            Content-Type: text/html\n
+            ConnectionType: keep-alive\n
+            Content-Length: 1\n\n
+
+            body
+        """
 
 IP = "127.0.0.1"
-PORT = 10000
+PORT = 10001
 SIZE = 1024
 
-while True:
-    with socket(AF_INET, SOCK_STREAM) as client_socket:
-        client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 연결
+def request():
+    return 1
 
-        method = input("Input method (GET, POST, PUT, HEAD): ")
-        url = input('Input URL : ')
-        client_socket.send((method + ',' + url).encode('utf-8'))
-        print("Sending Request to Server")
+with socket(AF_INET, SOCK_STREAM) as client_socket:
+    client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 연결
+    method = input("Input method (GET, POST, PUT, HEAD): ")
+    url = input('Input URL : ')
+    body = input('Input Data' : ')
 
-        data = client_socket.recv(SIZE).decode('utf-8')
-        print("Response data : ", data)
-        client_socket.close()
+    client_socket.send((method + ',' + url + ',' + body).encode('utf-8'))
+
+    data = client_socket.recv(SIZE).decode('utf-8')
+    print(data)
+    client_socket.close()
--- server.py
+++ server.py
@@ -1,42 +1,68 @@
 from socket import *
+import time
+format = """HTTP/1.1 200 OK\n
+            Content-Type: text/html\n
+            ConnectionType: keep-alive\n
+            Content-Length: 1\n
+            Date: Wed, 10 Aug 2016 09:23:12 GMT\n\n
 
+            body
+        """
 
 HOST = "127.0.0.1"
-PORT = 10000
+PORT = 10001
 SIZE = 1024
-SERVER_URL = "test.com"
-STATUS_CODE_OK = 'HTTP/200 OK'
-STATUS_CODE_BAD = 'HTTP/400 BAD REQUEST'
-STATUS_CODE_CREATED = 'HTTP/201 CREATED' 
 
-def get(url):
-    if url == SERVER_URL:
-        response = STATUS_CODE_OK
+CONTINUE = 0
+OK = 1
+CREATED = 2
+BAD_REQUEST = 3
+NOT_FOUND = 4
+STATUS_CODE = ['100', '200', '201', '400', '404']
+STATUS_MESSAGE = ['CONTINUE', 'OK', 'CREATED', 'BAD_REQUEST', 'NOT_FOUND']
+
+DB_DATA = {}
+
+def _response(status_code, status_msg, body=None):
+    date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime(time.time()))
+    return f"HTTP/1.1 {status_code} {status_msg}\nContent-Type: text/html\nConnectionType: keep-alive\nContent-Length: {len(body)}\nDate: {date}\n\n{body}"
+
+def router(url, method):
+    host, path = url.split('/')
+
+    if path == 'index.html': return get(method)
+    # elif path == 'create': return post()
+    # elif path == 'update': return put()
+    # elif method == 'HEAD': return head()
+
+def get(method):
+    if method == 'GET':
+        return _response(status_code=STATUS_CODE[OK],
+                        status_msg=STATUS_MESSAGE[OK],
+                        body='index.html')
     else:
-        response = STATUS_CODE_BAD
+        return _response(STATUS_CODE[BAD_REQUEST], STATUS_MESSAGE[BAD_REQUEST])
+
+def post(method, k, v):
+    if method == 'POST' and type(k) == 'str' and type(v) == 'str':
+        DB_DATA[k] = v
+        return _response(status_code=STATUS_CODE[CREATED],
+                        status_msg=STATUS_MESSAGE[CREATED],
+                        body=str(DB_DATA))
+    else:
+        return _response(STATUS_CODE[BAD_REQUEST], STATUS_MESSAGE[BAD_REQUEST])
     return response
 
-def post(url):
-    if url == SERVER_URL + '/create':
-        response = STATUS_CODE_CREATED
-    else:
-        response = STATUS_CODE_BAD
-    return response
+# def put(url):
+#     if url == HOST + '/update':
+#         response = STATUS_CODE['OK']
+#     return response
 
-def head(url):
-    if url == SERVER_URL + '/option':
-        response = STATUS_CODE_OK
-    else:
-        response = STATUS_CODE_BAD
-    return response
 
-def put(url):
-    if url == SERVER_URL + '/update':
-        response = STATUS_CODE_OK
-    else:
-        response = STATUS_CODE_BAD
-    return response
-
+# def head(url):
+#     if url == HOST + '/option':
+#         response = STATUS_CODE['BAD REQUEST']
+#     return response
 
 with socket(AF_INET, SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))  # 생성한 소켓에 HOST와 PORT 바인딩
@@ -45,20 +71,9 @@ with socket(AF_INET, SOCK_STREAM) as server_socket:
 
     while True:
         client_socket, client_addr = server_socket.accept()  # 소켓이 연결 될 떄 client의 소켓과 주소 반환
-        print(str(client_addr), "에서 접속 완료!")
 
         data = client_socket.recv(SIZE).decode('utf-8').split(',')  # client에서 보내는 데이터 받기
         method, url = data[0], data[1]
-
-        if method == 'GET':
-            response = get(url)
-        
-        elif method == 'POST':
-            response = post(url)
-
-        elif method == 'HEAD':
-            response = head(url)
-        else:
-            response = put(url)
-        print(f"Request Method/URL : {method} {url} - {response}")
+        body = data[2]
+        response = router(url, method, body)
         client_socket.send(response.encode('utf-8'))  # 데이터 인코딩하여 보내기
