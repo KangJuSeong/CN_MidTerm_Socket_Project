b9f7a29e900eee7ab417464231fef18e2e1861c1
KangJuSeong <wntjd3686@gmail.com>
2022-04-02 02:21:10 +0900

    write test case and run test case loop

--- client.py
+++ client.py
@@ -2,23 +2,53 @@ from socket import *
 
 
 IP = "127.0.0.1"
-PORT = 10001
+PORT = 10000
 SIZE = 1024
-
+test_case = [
+    {'url': '127.0.0.1/index.html',
+     'method': 'GET',
+     'body': ''
+    },
+    {'url': '127.0.0.1/test.html',
+     'method': 'GET',
+     'body': ''
+    },
+    {'url': '127.0.0.1/',
+     'method': 'HEAD',
+     'body': ''
+    },
+    {'url': '127.0.0.1/create',
+     'method': 'POST',
+     'body': 'name:kangjuseong'
+    },
+    {'url': '127.0.0.1/create',
+     'method': 'POST',
+     'body': 'test'
+    },
+    {'url': '127.0.0.1/update',
+     'method': 'PUT',
+     'body': 'name:juseong-kang'
+    },
+    {'url': '127.0.0.1/update',
+     'method': 'PUT',
+     'body': 'grade:10'
+    },
+    {'url': '127.0.0.1/test',
+     'method': 'POST',
+     'body': 'test:test'
+    }
+]
 def request_formating(method, body, url):
     return f"{method} / HTTP/1.1\r\nHost: {url}\r\nAccept: text/html\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nContent-Length: {len(body)}\r\n\n{body}"
 
-with socket(AF_INET, SOCK_STREAM) as client_socket:
-    client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 연결
-    method = input("Input method (GET, POST, PUT, HEAD): ")
-    url = input("Input URL : ")
-    body = ''
-    if method == 'POST' or method == 'PUT':
-        body = input('Input Data : ')
-    request = request_formating(method, body, url)
-
-    client_socket.send(request.encode('utf-8'))
-
-    response = client_socket.recv(SIZE).decode('utf-8')
-    print(response)
-    client_socket.close()
+for test in test_case:
+    with socket(AF_INET, SOCK_STREAM) as client_socket:
+        client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 연결
+        method = test['method']
+        url = test['url']
+        body = test['body']
+        request = request_formating(method, body, url)
+        client_socket.send(request.encode('utf-8'))
+        response = client_socket.recv(SIZE).decode('utf-8')
+        print(response)
+        client_socket.close()
