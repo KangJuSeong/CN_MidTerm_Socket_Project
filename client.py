74389d024ad2115b26e3eed220a3ec179a2bb1ba
KangJuSeong <wntjd3686@gmail.com>
2022-04-01 19:05:26 +0900

    compleate http1.1 formating and testing request, response

--- client.py
+++ client.py
@@ -1,30 +1,24 @@
 from socket import *
 
-format = """POST / HTTP/1.1\n
-            Host: 127.0.0.1:10000
-            Accept: text/html
-            Content-Type: text/html\n
-            ConnectionType: keep-alive\n
-            Content-Length: 1\n\n
-
-            body
-        """
 
 IP = "127.0.0.1"
 PORT = 10001
 SIZE = 1024
 
-def request():
-    return 1
+def request_formating(method, body, url):
+    return f"{method} / HTTP/1.1\r\nHost: {url}\r\nAccept: text/html\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nContent-Length: {len(body)}\r\n\n{body}"
 
 with socket(AF_INET, SOCK_STREAM) as client_socket:
     client_socket.connect((IP, PORT))  # 생성한 소켓에 HOST와 PORT 연결
     method = input("Input method (GET, POST, PUT, HEAD): ")
-    url = input('Input URL : ')
-    body = input('Input Data' : ')
+    url = input("Input URL : ")
+    body = ''
+    if method == 'POST' or method == 'PUT':
+        body = input('Input Data : ')
+    request = request_formating(method, body, url)
 
-    client_socket.send((method + ',' + url + ',' + body).encode('utf-8'))
+    client_socket.send(request.encode('utf-8'))
 
-    data = client_socket.recv(SIZE).decode('utf-8')
-    print(data)
+    response = client_socket.recv(SIZE).decode('utf-8')
+    print(response)
     client_socket.close()
--- server.py
+++ server.py
@@ -23,57 +23,87 @@ STATUS_MESSAGE = ['CONTINUE', 'OK', 'CREATED', 'BAD_REQUEST', 'NOT_FOUND']
 
 DB_DATA = {}
 
-def _response(status_code, status_msg, body=None):
+
+def find_http_method(line):
+    line = line.split(' ')
+    return line[0]
+    
+def response_formating(status_code, status_msg, body=''):
     date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.localtime(time.time()))
-    return f"HTTP/1.1 {status_code} {status_msg}\nContent-Type: text/html\nConnectionType: keep-alive\nContent-Length: {len(body)}\nDate: {date}\n\n{body}"
+    return f"HTTP/1.1 {status_code} {status_msg}\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nContent-Length: {len(body)}\r\nDate: {date}\r\n\n{body}"
 
-def router(url, method):
-    host, path = url.split('/')
+def _response(status, body=''):
+    if status == CONTINUE:
+        return response_formating(STATUS_CODE[CONTINUE], STATUS_MESSAGE[CONTINUE], body)
+    if status == OK:
+        return response_formating(STATUS_CODE[OK], STATUS_MESSAGE[OK], body)
+    elif status == CREATED:
+        return response_formating(STATUS_CODE[CREATED], STATUS_MESSAGE[CREATED], body)
+    elif status == BAD_REQUEST:
+        return response_formating(STATUS_CODE[BAD_REQUEST], STATUS_MESSAGE[BAD_REQUEST], body)
+    if status == NOT_FOUND:
+        return response_formating(STATUS_CODE[NOT_FOUND], STATUS_MESSAGE[NOT_FOUND], body)
+    
 
-    if path == 'index.html': return get(method)
-    # elif path == 'create': return post()
-    # elif path == 'update': return put()
-    # elif method == 'HEAD': return head()
-
-def get(method):
-    if method == 'GET':
-        return _response(status_code=STATUS_CODE[OK],
-                        status_msg=STATUS_MESSAGE[OK],
-                        body='index.html')
+def router(url, method, body):
+    if '/' in url:
+        host, path = url.split('/')
+        if host == HOST:
+            if method == 'HEAD': return head()
+            if path == 'index.html':
+                if method == 'GET': return get()
+                else: return _response(BAD_REQUEST)
+            elif path == 'create':
+                if method == 'POST': return post(body)
+                else: return _response(BAD_REQUEST)
+            elif path == 'update':
+                if method == 'PUT': return put(body)
+                else: return _response(BAD_REQUEST)
+            else: return _response(NOT_FOUND)
+        else:
+            return ''
     else:
-        return _response(STATUS_CODE[BAD_REQUEST], STATUS_MESSAGE[BAD_REQUEST])
+        return _response(NOT_FOUND)
 
-def post(method, k, v):
-    if method == 'POST' and type(k) == 'str' and type(v) == 'str':
+def get():
+    return _response(OK, body='index.html')
+
+def head():
+    return _response(CONTINUE) 
+
+def post(body):
+    k, v = body.split(':')
+    if type(k) is str and type(v) is str:
         DB_DATA[k] = v
-        return _response(status_code=STATUS_CODE[CREATED],
-                        status_msg=STATUS_MESSAGE[CREATED],
-                        body=str(DB_DATA))
+        return _response(CREATED, body=str(DB_DATA))
     else:
-        return _response(STATUS_CODE[BAD_REQUEST], STATUS_MESSAGE[BAD_REQUEST])
-    return response
+        return _response(BAD_REQUEST)
 
-# def put(url):
-#     if url == HOST + '/update':
-#         response = STATUS_CODE['OK']
-#     return response
+def put(body):
+    k, v = body.split(':')
+    if type(k) is str and type(v) is str:
+        if k in DB_DATA.keys():
+            DB_DATA[k] = v
+            return _response(OK, body=str(DB_DATA))
+        else:
+            return _response(BAD_REQUEST, body='Not Exist Data')
+    else:
+        return _response(BAD_REQUEST)
 
-
-# def head(url):
-#     if url == HOST + '/option':
-#         response = STATUS_CODE['BAD REQUEST']
-#     return response
 
 with socket(AF_INET, SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))  # 생성한 소켓에 HOST와 PORT 바인딩
     server_socket.listen(1)  # 소켓 연결 대기 상태
-    print("소켓이 대기중 입니다...")
 
     while True:
         client_socket, client_addr = server_socket.accept()  # 소켓이 연결 될 떄 client의 소켓과 주소 반환
 
-        data = client_socket.recv(SIZE).decode('utf-8').split(',')  # client에서 보내는 데이터 받기
-        method, url = data[0], data[1]
-        body = data[2]
+        data = client_socket.recv(SIZE).decode('utf-8')  # client에서 보내는 데이터 받기
+        print(data)
+        data = data.split('\n')
+        method = find_http_method(data[0])
+        url = data[1][6:-1]
+        body = data[-1]
         response = router(url, method, body)
+
         client_socket.send(response.encode('utf-8'))  # 데이터 인코딩하여 보내기
